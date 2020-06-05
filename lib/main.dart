1eeec5dfc034a971446c5780e087333b369c6b08
Faith Korir <faithckorir@gmail.com>
2020-06-05 22:56:01 +0300

    app changed to use statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -14,10 +16,50 @@ void main() {
     ),
   );
 }
-
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber=1;
+  int rightDiceNumber=1;
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+
+            child: FlatButton(onPressed: (){
+              setState(() {
+                leftDiceNumber=Random().nextInt(6)+1;
+
+              });
+
+            },
+              child: Image.asset(
+                ('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+
+          ),
+          Expanded(
+            child: FlatButton(onPressed: (){setState(() {
+              rightDiceNumber=Random().nextInt(6)+1;
+            });
+
+            },
+              child: Image.asset(
+                ('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
+
+
